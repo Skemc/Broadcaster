f05b9b9a3139293688bd0b12c7bbdf544a1f041e
Skemc <eric.mico6@gmail.com>
2019-11-07 19:48:33 +0200

    Chore(UI): Update Responsiveness - Update UI pages - make the the app more responsive [Finishes: #169621486]

--- UI/script/scripts.js
+++ UI/script/scripts.js
@@ -15,10 +15,3 @@ var loadFiles = function(event) {
 	var video = document.getElementById('output1');
 	video.src = URL.createObjectURL(event.target.files[0]);
 };
-
-function initMap() {
-    var rwanda = {lat: -1.9496959999999999, lng:  30.1006848};
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: rwanda});
-    var marker = new google.maps.Marker({position: rwanda, map: map});
-  } 
