e64dc9217f5aac684d683b161abd32e1ebbf47d9
Skemc <eric.mico6@gmail.com>
2019-11-16 16:33:21 +0200

    ft(signup): create user - add validation - add signup test - set up travis and coveralls badge - add readme badge [Finishes #169740688]

---
+++ Server/tests/mockdata/mockdata.js
@@ -0,0 +1,12 @@
+const mock = {
+    signup: {
+    firstName: 'ookkrae',
+    lastName: 'eric',
+    email: 'eric@gmail.com',
+    password: 'rwandaaa',
+    userName: 'eric',
+    phoneNumber: '09484884'
+
+}};
+
+export default mock;
